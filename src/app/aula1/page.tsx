f7c7b6124084cb62b450636e2bd361db8d983f97
raphaeldamsaceno07 <root@DESKTOP-JAF4DBA>
2024-04-18 22:14:19 -0300

    aula 1 2 e 3

---
+++ src/app/aula1/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div>
+      <div className="border-2 border-r-blue-300 border-l-green-300 border-t-gray-300 border-b-vermelhoEscuro">Olá Mundo!</div>
+    </div>
+  )
+}
+export default Page
